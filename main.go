6b95308ddf0590bb668b392acc9720619aec4f92
Oliver Smith <ollieparanoid@bitmessage.ch>
2018-12-20 09:53:52 +0100

    Add aliases: art, bpo, chrg, lnx, mrh, osk, wiki
    The wiki does not have merge requests, hence wiki! is not added.

--- main.go
+++ main.go
@@ -48,12 +48,34 @@ func main() {
 	fmt.Println("Login successful")
 
 	shortcutmap := map[string]string{
+		"art#": "https://gitlab.com/postmarketOS/artwork/issues/",
+		"art!": "https://gitlab.com/postmarketOS/artwork/merge_requests/",
+
+		"bpo#": "https://gitlab.com/postmarketOS/build.postmarketos.org/issues/",
+		"bpo!": "https://gitlab.com/postmarketOS/build.postmarketos.org/merge_requests/",
+
+		"chrg!": "https://gitlab.com/postmarketOS/charging-sdl/issues/",
+		"chrg#": "https://gitlab.com/postmarketOS/charging-sdl/merge_requests/",
+
+		"lnx!": "https://gitlab.com/postmarketOS/linux-postmarketos/issues/",
+		"lnx#": "https://gitlab.com/postmarketOS/linux-postmarketos/merge_requests/",
+
+		"mrh!": "https://gitlab.com/postmarketOS/mrhlpr/issues/",
+		"mrh#": "https://gitlab.com/postmarketOS/mrhlpr/merge_requests/",
+
+		"osk!": "https://gitlab.com/postmarketOS/osk-sdl/issues/",
+		"osk#": "https://gitlab.com/postmarketOS/osk-sdl/merge_requests/",
+
 		"pma#": "https://gitlab.com/postmarketOS/pmaports/issues/",
 		"pma!": "https://gitlab.com/postmarketOS/pmaports/merge_requests/",
+
 		"pmb#": "https://gitlab.com/postmarketOS/pmbootstrap/issues/",
 		"pmb!": "https://gitlab.com/postmarketOS/pmbootstrap/merge_requests/",
+
 		"org#": "https://gitlab.com/postmarketOS/postmarketos.org/issues/",
 		"org!": "https://gitlab.com/postmarketOS/postmarketos.org/merge_requests/",
+
+		"wiki#": "https://gitlab.com/postmarketOS/wiki/merge_requests/",
 	}
 	shortcutmapregex := regexp.MustCompile("(?i)(pma[#!]|pmb[#!]|org[#!])(\\d+)")
 
